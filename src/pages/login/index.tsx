d9e61375c234feec439dfbd0cb616dc89e0ccc9e
dhzhang <zhangdlmj@163.com>
2024-05-14 07:28:18 +0800

    feat: fetch加上timeout

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useDispatch } from 'react-redux'
 import { useNavigate } from 'react-router'
 import { setUserInfo } from '../../store/user'
+import {request} from "../../utils/http";
 
 type Props = {}
 
@@ -9,13 +10,21 @@ export default function Login({}: Props) {
   const navigate = useNavigate()
   const dispatch = useDispatch()
   const login = () => {
-    dispatch(setUserInfo({token:"1231111"}))
-    navigate("/")
+    // dispatch(setUserInfo({token:"1231111"}))
+    request.post("/user/login", {method:"POST"}).then(res => res.json()).then(result => {
+      console.log(111,{result})
+    })
+    // navigate("/")
+  }
+  const testFetch = async() => {
+    const promise = fetch("user/login", {method:"POST"})
+    console.log(promise.finally())
   }
   return (
     <div>
       Login 
       <button onClick={login}>跳转首页</button>
+      <button onClick={testFetch}>点击</button>
     </div>
   )
 }
--- src/utils/http.tsx
+++ src/utils/http.tsx
@@ -1,25 +1,59 @@
+type inputValue = string | URL | Request
+type Options =  RequestInit | undefined
+type Timeout = {
+    timeout?: number
+}
+type RequestOptions = {
+    timeout?: number
+}
+
 class HttpClient {
-    private baseURL: string;
-    private mode: string;
-    private http = fetch
-    constructor(baseURL: string, mode: string) {
-        this.baseURL = baseURL;
-        this.mode = mode;
-    }
-    post(url: string, data?: any): Promise<any> {
-        return new Promise((resolve, reject) => {})
-    }
-    get(url: string, data?: any): Promise<any> {
-        return new Promise((resolve, reject) => {})
-    }
-
-    delete(url: string, data?: any): Promise<any> {
-        return new Promise((resolve, reject) => {})
+    private timeout: number = 30000;
+    // 默认60秒去取消请求
+    private fetchWidthTimeout(url:inputValue,  { signal, timeout, ...options }:Options&Timeout = {}) {
+        if(!timeout){
+            timeout = this.timeout;
+        }
+        const controller = new AbortController();
+        const promise = fetch(url, { signal: controller.signal, ...options });
+        if (signal) signal.addEventListener("abort", () => controller.abort());
+        const timer = setTimeout(() => controller.abort(), timeout);
+        return promise.finally(() => clearTimeout(timer));
     }
 
-    put(url: string, data?: any): Promise<any> {
-        return new Promise((resolve, reject) => {})
+    post(url: inputValue, data: any, options?:RequestOptions&Options): Promise<any> {
+        return this.fetchWidthTimeout(url, {
+            ...options,
+            method: 'POST',
+            body: JSON.stringify(data),
+            credentials: 'include'
+        })
+    }
+
+    get(url: inputValue, options?:RequestOptions&Options): Promise<any> {
+        return this.fetchWidthTimeout(url, {
+            ...options,
+            method: 'GET',
+            credentials: 'include'
+        })
+    }
+
+    delete(url: inputValue, options?:RequestOptions&Options): Promise<any> {
+        return this.fetchWidthTimeout(url, {
+            ...options,
+            credentials:'include',
+            method: 'DELETE',
+        })
+    }
+
+    put(url: inputValue, data?: any, options?:RequestOptions&Options): Promise<any> {
+        return this.fetchWidthTimeout(url, {
+            ...options,
+            body: JSON.stringify(data),
+            credentials: 'include',
+            method:"PUT"
+        })
     }
 }
 
-export const request = new HttpClient("/api", import.meta.env.MODE)
+export const request = new HttpClient()
